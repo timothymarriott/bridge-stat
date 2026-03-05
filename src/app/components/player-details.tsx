c059a29882478822ac63ea585bee87abe16cc67a
Timothy Marriott <timothyrmarriott@gmail.com>
2026-03-05 13:24:00 +0800

    graphs

--- src/app/components/player-details.tsx
+++ src/app/components/player-details.tsx
@@ -4,15 +4,31 @@ import {
 	ChartTooltip,
 	ChartTooltipContent,
 } from "@/components/ui/chart";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { EloInformation, GetELO, MatchEloInformation } from "@/lib/stats";
 import { Match, OptionalPlayerInformation, PlayerInformation } from "@/worker/types";
 import { Area, AreaChart, CartesianGrid, XAxis, YAxis } from "recharts";
 import { Rating } from "ts-trueskill";
 
-const chartConfig = {
+const eloChartConfig = {
 	elo: {
 		label: "Elo",
-		color: "#2563eb",
+		color: "var(--chart-1)",
+	},
+	confidence: {
+		label: "Elo",
+		color: "var(--chart-2)",
+	},
+} satisfies ChartConfig;
+
+const winsChartConfig = {
+	win: {
+		label: "Win Rate",
+		color: "var(--color-green-400)",
+	},
+	loss: {
+		label: "Loss Rate",
+		color: "var(--color-red-400)",
 	},
 } satisfies ChartConfig;
 
@@ -27,7 +43,7 @@ export default function PlayerDetails({
 	players: OptionalPlayerInformation[];
 	matches: Record<string, Match>;
 }) {
-	const chartData: {
+	const eloChartData: {
 		date: number;
 		elo: number;
 		confidence: number;
@@ -35,104 +51,254 @@ export default function PlayerDetails({
 		info: MatchEloInformation;
 	}[] = [];
 
+	const winsChartData: {
+		date: number;
+		win: number;
+		loss: number;
+	}[] = [];
+
+	let totalWin = 0;
+	let totalLoss = 0;
+
+	let totalMatches = 0;
+
+	let totalKill = 0;
+	let totalDeath = 0;
+
 	Object.values(elos.matches).forEach((v) => {
-		if (v.totals[player.id] != undefined) {
-			chartData.push({
-				date: chartData.length,
+		const performance = [...v.match.blue_players, ...v.match.red_players].find(
+			(p) => p.user == player.id,
+		);
+		if (v.totals[player.id] != undefined && performance != undefined) {
+			totalMatches++;
+			eloChartData.push({
+				date: eloChartData.length,
 				elo: GetELO(v.totals[player.id]),
 				confidence: v.totals[player.id].pi * 1500,
 				rating: v.totals[player.id],
 				info: v,
 			});
+
+			totalKill += performance.kills;
+			totalDeath += performance.deaths;
+
+			if (v.match.blue_scores == 5 && v.match.blue_players.find((p) => p.user == player.id)) {
+				totalWin += 1;
+			}
+			if (v.match.red_scores == 5 && v.match.red_players.find((p) => p.user == player.id)) {
+				totalWin += 1;
+			}
+
+			if (
+				v.match.blue_scores == 5 &&
+				v.match.blue_players.find((p) => p.user == player.id) == undefined
+			) {
+				totalLoss += 1;
+			}
+			if (
+				v.match.red_scores == 5 &&
+				v.match.red_players.find((p) => p.user == player.id) == undefined
+			) {
+				totalLoss += 1;
+			}
+
+			winsChartData.push({
+				date: winsChartData.length,
+				win: totalWin / totalMatches,
+				loss: totalLoss / totalMatches,
+			});
 		}
 	});
 	return (
 		<>
-			<ChartContainer config={chartConfig}>
-				<AreaChart
-					accessibilityLayer
-					data={chartData}
-					margin={{
-						left: 12,
-						right: 12,
-					}}
-				>
-					<CartesianGrid vertical={false} />
-					<XAxis dataKey="date" tickMargin={8} />
-					<YAxis dataKey="elo" tickMargin={8} />
-					<ChartTooltip
-						cursor={true}
-						content={
-							<ChartTooltipContent
-								hideLabel
-								formatter={(value, name, item, index) => {
-									if (index == 0) {
-										const payload = item.payload as {
-											date: number;
-											elo: number;
-											confidence: number;
-											rating: Rating;
-											info: MatchEloInformation;
-										};
-										return (
-											<div className="space-x-1">
-												<span>
-													{Math.floor(GetELO(payload.rating))} Elo
-												</span>
-												<span
-													className={
-														Math.floor(payload.info.deltas[player.id]) >
-														0
-															? "text-green-400"
-															: "text-red-400"
-													}
-												>
-													{Math.floor(payload.info.deltas[player.id]) > 0
-														? "+"
-														: " "}
-													{Math.abs(
-														Math.floor(payload.info.deltas[player.id]),
-													)}
-												</span>
-												<br />
-												<span>
-													{Math.round(payload.rating.pi * 100)}% Confident
-												</span>
-											</div>
-										);
-									}
-								}}
+			<Tabs defaultValue="elo">
+				<TabsList>
+					<TabsTrigger value="elo">Elo</TabsTrigger>
+					<TabsTrigger value="wins">Game Results</TabsTrigger>
+				</TabsList>
+				<TabsContent value="elo">
+					<ChartContainer config={eloChartConfig}>
+						<AreaChart
+							accessibilityLayer
+							data={eloChartData}
+							margin={{
+								left: 12,
+								right: 12,
+							}}
+						>
+							<CartesianGrid vertical={false} />
+							<XAxis dataKey="date" tickMargin={8} />
+							<YAxis dataKey="elo" tickMargin={8} />
+							<ChartTooltip
+								cursor={true}
+								content={
+									<ChartTooltipContent
+										hideLabel
+										formatter={(value, name, item, index) => {
+											if (index == 0) {
+												const payload = item.payload as {
+													date: number;
+													elo: number;
+													confidence: number;
+													rating: Rating;
+													info: MatchEloInformation;
+												};
+												return (
+													<div className="space-x-1">
+														<span>
+															{Math.floor(GetELO(payload.rating))} Elo
+														</span>
+														<span
+															className={
+																Math.floor(
+																	payload.info.deltas[player.id],
+																) > 0
+																	? "text-green-400"
+																	: "text-red-400"
+															}
+														>
+															{Math.floor(
+																payload.info.deltas[player.id],
+															) > 0
+																? "+"
+																: "-"}
+															{Math.abs(
+																Math.floor(
+																	payload.info.deltas[player.id],
+																),
+															)}
+														</span>
+														<br />
+														<span>
+															{Math.round(payload.rating.pi * 100)}%
+															Confident
+														</span>
+													</div>
+												);
+											}
+										}}
+									/>
+								}
 							/>
-						}
-					/>
-					<defs>
-						<linearGradient id="fillElo" x1="0" y1="0" x2="0" y2="1">
-							<stop offset="5%" stopColor="var(--chart-1)" stopOpacity={0.8} />
-							<stop offset="95%" stopColor="var(--chart-1)" stopOpacity={0.1} />
-						</linearGradient>
-						<linearGradient id="fillConfidence" x1="0" y1="0" x2="0" y2="1">
-							<stop offset="5%" stopColor="var(--chart-2)" stopOpacity={0.8} />
-							<stop offset="95%" stopColor="var(--chart-2)" stopOpacity={0.1} />
-						</linearGradient>
-					</defs>
-					<Area
-						dataKey="elo"
-						type="natural"
-						fill="url(#fillElo)"
-						fillOpacity={0.4}
-						stroke="var(--chart-1)"
-						stackId="a"
-					/>
-					<Area
-						dataKey="confidence"
-						type="natural"
-						fill="url(#fillConfidence)"
-						fillOpacity={0.4}
-						stroke="var(--chart-2)"
-						stackId="b"
-					/>
-				</AreaChart>
-			</ChartContainer>
+							<defs>
+								<linearGradient id="fillElo" x1="0" y1="0" x2="0" y2="1">
+									<stop
+										offset="5%"
+										stopColor="var(--chart-1)"
+										stopOpacity={0.8}
+									/>
+									<stop
+										offset="95%"
+										stopColor="var(--chart-1)"
+										stopOpacity={0.1}
+									/>
+								</linearGradient>
+								<linearGradient id="fillConfidence" x1="0" y1="0" x2="0" y2="1">
+									<stop
+										offset="5%"
+										stopColor="var(--chart-2)"
+										stopOpacity={0.8}
+									/>
+									<stop
+										offset="95%"
+										stopColor="var(--chart-2)"
+										stopOpacity={0.1}
+									/>
+								</linearGradient>
+							</defs>
+							<Area
+								dataKey="elo"
+								type="linear"
+								fill="url(#fillElo)"
+								fillOpacity={0.4}
+								stroke="var(--chart-1)"
+								stackId="a"
+							/>
+							<Area
+								dataKey="confidence"
+								type="natural"
+								fill="url(#fillConfidence)"
+								fillOpacity={0.4}
+								stroke="var(--chart-2)"
+								stackId="b"
+							/>
+						</AreaChart>
+					</ChartContainer>
+					<span className="text-accent-foreground/50">
+						This is the players rating according to the TrueSkill2 system. <br /> The
+						numeric value is calculated via this formula x = μ * 100 + 1000.
+						<br /> The confidence is π treated as a percentage.
+					</span>
+				</TabsContent>
+				<TabsContent value="wins">
+					<ChartContainer config={winsChartConfig}>
+						<AreaChart
+							accessibilityLayer
+							data={winsChartData}
+							margin={{
+								left: 12,
+								right: 12,
+							}}
+						>
+							<CartesianGrid vertical={false} />
+							<XAxis dataKey="date" tickMargin={8} label={"Match"} />
+							<YAxis dataKey="win" tickMargin={8} />
+							<ChartTooltip
+								cursor={true}
+								content={<ChartTooltipContent hideLabel />}
+							/>
+							<defs>
+								<linearGradient id="fillWin" x1="0" y1="0" x2="0" y2="1">
+									<stop
+										offset="5%"
+										stopColor="var(--color-green-400)"
+										stopOpacity={0.8}
+									/>
+									<stop
+										offset="95%"
+										stopColor="var(--color-green-400)"
+										stopOpacity={0.1}
+									/>
+								</linearGradient>
+								<linearGradient id="fillLoss" x1="0" y1="1" x2="0" y2="0">
+									<stop
+										offset="5%"
+										stopColor="var(--color-red-400)"
+										stopOpacity={0.8}
+									/>
+									<stop
+										offset="95%"
+										stopColor="var(--color-red-400)"
+										stopOpacity={0.1}
+									/>
+								</linearGradient>
+							</defs>
+							<Area
+								dataKey="win"
+								type="natural"
+								fill="url(#fillWin)"
+								fillOpacity={0.4}
+								strokeWidth={2}
+								stroke="var(--color-green-400)"
+								stackId="a"
+							/>
+							<Area
+								dataKey="loss"
+								type="natural"
+								fill="url(#fillLoss)"
+								fillOpacity={0.4}
+								strokeOpacity={0}
+								activeDot={<></>}
+								stroke="var(--color-red-400)"
+								stackId="a"
+							/>
+						</AreaChart>
+					</ChartContainer>
+					<span className="text-accent-foreground/50">
+						This is a rolling average of the players win rate.
+					</span>
+				</TabsContent>
+			</Tabs>
 		</>
 	);
 }
